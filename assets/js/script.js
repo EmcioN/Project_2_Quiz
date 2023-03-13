18816a59292b4222cc01974affea4f54d3ce5ffb
EmcioN <villly1990@gmail.com>
2023-03-13 17:30:30 +0000

    Add Event listeners to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 const playButton = document.querySelector('.button[aria-label="Play"]');
 const endGameButton = document.querySelector('.button[aria-label="End game"]');
 const playAgainButton = document.querySelector('.button[aria-label="Play again"]');
+const homeButton = document.querySelector('.button[aria-label="Home"]');
+const scoreButton = document.querySelector('.button[aria-label="Your Score"]');
+const yourScoreButton = document.querySelector('.button[aria-label="Your Scores"]');
 const mainSection = document.getElementById('main');
 const gameSection = document.getElementById('game');
 const endGameSection = document.getElementById('end-game');
+const scoreSection = document.getElementById('scores');
 
 playButton.addEventListener('click', () => {
     mainSection.style.display = 'none';
@@ -19,6 +23,24 @@ playAgainButton.addEventListener('click', () => {
     mainSection.style.display = 'flex';
     gameSection.style.display = 'none';
   });
+scoreButton.addEventListener('click', () => {
+    endGameSection.style.display = 'none';
+    mainSection.style.display = 'none';
+    gameSection.style.display = 'none';
+    scoreSection.style.display = 'flex';
+  });
+yourScoreButton.addEventListener('click', () => {
+    endGameSection.style.display = 'none';
+    mainSection.style.display = 'none';
+    gameSection.style.display = 'none';
+    scoreSection.style.display = 'flex';
+  });
+homeButton.addEventListener('click', () => {
+    endGameSection.style.display = 'none';
+    mainSection.style.display = 'flex';
+    gameSection.style.display = 'none';
+    scoreSection.style.display = 'none';
+  });    
 
 // Questions
 
